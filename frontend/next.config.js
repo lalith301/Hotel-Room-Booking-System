decc760e4f38cf8e43713be00b1d2e17be19e911
lalith301 <lkmm334@gmail.com>
2025-07-05 23:31:35 +0530

    next.config.js

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,12 +1,20 @@
+/** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  eslint: {
-    ignoreDuringBuilds: true // Add this line
-  },
   publicRuntimeConfig: {
     // Will be available on both server and client
-    API_BASE_URL: process.env.API_BASE_URL
+    API_BASE_URL: process.env.API_BASE_URL || 'https://hotel-room-booking-system-j0rb.onrender.com',
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  transpilePackages: ['antd', 'rc-util'],
+  experimental: {
+    esmExternals: 'loose'
+  },
+  env: {
+    API_BASE_URL: process.env.API_BASE_URL,
   }
-};
+}
 
 module.exports = nextConfig;
